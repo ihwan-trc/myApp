b6a51199182fde6edd268848f898ebee4b8a8f6c
ihwan-trc <ihwan.2705@gmail.com>
2022-06-11 03:06:09 +0700

    create seeder permissions user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserTableSeeder::class,
             CategoryTableSeeder::class,
             TagTableSeeder::class,
+            PermissionTableSeeder::class
         ]);
     }
 }
